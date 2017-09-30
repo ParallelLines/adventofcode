4c5f3d320df052168e26c89983bd8917efaa3a92
ParallelLines <tishkane@yandex.ru>
2017-10-01 01:42:03 +0400

    day 1 | part 2 is solved

--- src/main/java/sixteen/One.java
+++ src/main/java/sixteen/One.java
@@ -7,6 +7,8 @@ import javafx.util.Pair;
 import java.util.LinkedList;
 
 import static java.lang.Math.abs;
+import static java.lang.Math.max;
+import static java.lang.Math.min;
 
 /**
  * Created by parallellines on 29.09.2017.
@@ -55,18 +57,34 @@ public class One extends Task<Integer> {
         private Direction currentDirection;
         private Pair<Integer, Integer> startPosition;
         private Pair<Integer, Integer> position;
+        private LinkedList<Segment> allSegments;
+        private Pair<Integer, Integer> firstPositionVisitedTwice = null;
+
 
         Me(Direction currentDirection, Pair<Integer, Integer> position) {
             this.currentDirection = currentDirection;
             this.startPosition = position;
             this.position = position;
+            allSegments = new LinkedList<>();
         }
 
         void move(String instruction) {
-            currentDirection = getDirection(instruction);
-            Integer newX = position.getKey() + currentDirection.getX() * getNumberOfSteps(instruction);
-            Integer newY = position.getValue() + currentDirection.getY() * getNumberOfSteps(instruction);
-            position = new Pair<>(newX, newY);
+            if (firstPositionVisitedTwice == null) {
+                currentDirection = getDirection(instruction);
+                Integer newX = position.getKey() + currentDirection.getX() * getNumberOfSteps(instruction);
+                Integer newY = position.getValue() + currentDirection.getY() * getNumberOfSteps(instruction);
+                Segment newSegment = new Segment(position.getKey(), position.getValue(), newX, newY);
+                position = new Pair<>(newX, newY);
+                for (Segment segment : allSegments) {
+                    Pair<Integer, Integer> crossPoint = segment.doesCross(newSegment);
+                    if (crossPoint != null) {
+                        firstPositionVisitedTwice = crossPoint;
+                        position = crossPoint;
+                    }
+                }
+                allSegments.add(newSegment);
+            }
+
         }
 
         private Integer getNumberOfSteps(String instruction) {
@@ -105,6 +123,66 @@ public class One extends Task<Integer> {
             Integer projectionX = abs(startPosition.getKey()) - abs(position.getKey());
             Integer projectionY = abs(startPosition.getValue()) - abs(position.getValue());
             return abs(projectionX) + abs(projectionY);
+        }
+    }
+
+    class Segment {
+
+        private Integer startX;
+        private Integer startY;
+        private Integer endX;
+        private Integer endY;
+
+        Segment(Integer startX, Integer startY, Integer endX, Integer endY) {
+            this.startX = startX;
+            this.startY = startY;
+            this.endX = endX;
+            this.endY = endY;
+        }
+
+        private boolean isVertical(Segment segment) {
+            return segment.getStartX().equals(segment.getEndX());
+        }
+
+        private Segment getVertical(Segment one, Segment two) {
+            if (isVertical(one))
+                return one;
+            return two;
+        }
+
+        private Segment getHorizontal(Segment one, Segment two) {
+            if (!isVertical(one))
+                return one;
+            return two;
+        }
+
+        public Pair<Integer, Integer> doesCross(Segment otherSegment) {
+            if (isVertical(this) ^ isVertical(otherSegment)) {
+                Segment vertical = getVertical(this, otherSegment);
+                Segment horizontal = getHorizontal(this, otherSegment);
+                if (vertical.getStartX() > min(horizontal.getStartX(), horizontal.endX) &&
+                        vertical.getStartX() < max(horizontal.getStartX(), horizontal.endX) &&
+                        horizontal.getStartY() > min(vertical.getStartY(), vertical.getEndY()) &&
+                        horizontal.getStartY() < max(vertical.getStartY(), vertical.getEndY()))
+                    return new Pair<>(vertical.getStartX(), horizontal.getStartY());
+            }
+            return null;
+        }
+
+        public Integer getStartX() {
+            return startX;
+        }
+
+        public Integer getStartY() {
+            return startY;
+        }
+
+        public Integer getEndX() {
+            return endX;
+        }
+
+        public Integer getEndY() {
+            return endY;
         }
     }
 }
